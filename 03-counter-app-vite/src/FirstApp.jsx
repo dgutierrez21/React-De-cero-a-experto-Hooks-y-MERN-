1155171f6b87636242bff2dffd24c45f1080756a
Dixon G <dxxn0101@gmail.com>
2023-01-05 11:09:10 -0600

    Impresión de variables en el HTML

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,33 @@
+const nuevoMensaje = {
+  mensaje: "Hola Mundo obj",
+  autor: "Pedro",
+  enviarMensaje: false,
+};
+
+const autores = ["Pedro", "Juan", "Fernando", false, 10];
+
+const miFunc = (nombre) => `Hola ${nombre}`;
+
+const booleano = true;
+
+const primitivo = 30 * 6;
+
 export const FirstApp = () => {
+  // si no hay ninguna dependencia del componente a las funciones o viceversa, se recomienda poner las varibles fuera del componente
   return (
     <>
       <h1>FirstApp</h1>
-      <h1>FirstApp</h1>
+      <h2>Soy una función: {miFunc("Alberto")} </h2>
+      <h2>Soy un valor primitivo:{primitivo} </h2>
+      <h2>Soy un arreglo: {autores} </h2>
+      <h2>Soy un arreglo: {String(autores)} </h2>
+      <h2>Soy un booleano: {booleano} </h2>
+      <h2>Soy un booleano: {String(booleano)} </h2>
+      <h2>
+        Soy un objeto <code>{JSON.stringify(nuevoMensaje)}</code>
+      </h2>
+      <h2>Soy un objeto: {nuevoMensaje.mensaje}</h2>
+      <h2>Soy un objeto: {String(nuevoMensaje.enviarMensaje)}</h2>
     </>
   );
 };
