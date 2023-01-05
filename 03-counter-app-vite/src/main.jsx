a4bc434eea1f9104ffa21206fed3e2b081f1d577
Dixon G <dxxn0101@gmail.com>
2023-01-05 08:47:16 -0600

    Hola Mundo en React

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function App() {
+  // document.createElement
+
+  return <h1>Hola Mundo</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
