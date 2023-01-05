4da098df49c33723cf71bc7c878e3a527aab8a44
Dixon G <dxxn0101@gmail.com>
2023-01-05 09:00:41 -0600

    Nuestro primer Componente

---
+++ 03-counter-app-vite/src/App.jsx
@@ -0,0 +1,11 @@
+// function App() {
+//   // document.createElement
+
+//   return <h1>Hola Mundo</h1>;
+// }
+
+export const App = () => {
+  return <h1>Hola Mundo en React</h1>;
+};
+
+export default App;
