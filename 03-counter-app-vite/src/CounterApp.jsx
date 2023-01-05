09e49fb78ca821c61631f68ac0c7aeae9766836c
Dixon G <dxxn0101@gmail.com>
2023-01-05 14:23:56 -0600

    Evento click (Eventos en general)

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
 
+const handleAdd = (event) => console.log(event);
+
 const CounterApp = ({ value }) => {
+  // si una funcion no ocupa nada del componente en el cual esta alojada, se recomienda tenerla fuera del componente
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
