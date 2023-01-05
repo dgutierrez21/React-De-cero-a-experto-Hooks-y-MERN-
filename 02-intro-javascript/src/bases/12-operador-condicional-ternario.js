5aef585e9f90532c5ad090d9428f2c3db27984b1
Dixon G <dxxn0101@gmail.com>
2023-01-04 21:00:17 -0600

    operador condicional ternario

---
+++ 02-intro-javascript/src/bases/12-operador-condicional-ternario.js
@@ -0,0 +1,23 @@
+console.log("Hola Nundo");
+
+// operador condicional ternario
+// es una manera corta de hacer una definición
+
+const activo = false;
+
+// let mensaje;
+
+// if (activo) {
+//   mensaje = "Activo";
+// } else {
+//   mensaje = "Inactivo";
+// }
+
+const mensaje = activo ? "Activo" : "Inactivo";
+
+// solo ejecutar el if
+const mensaje2 = activo && "Activo";
+
+console.log(mensaje);
+
+console.log(mensaje2);
