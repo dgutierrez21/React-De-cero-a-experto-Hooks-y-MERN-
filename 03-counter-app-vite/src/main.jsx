b8d79e6d65e70bd6ef5a5d79949345ee92ee1e38
Dixon G <dxxn0101@gmail.com>
2023-01-05 14:03:33 -0600

    Tarea - Componente CounterApp

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import FirstApp from "./FirstApp";
+import CounterApp from "./CounterApp";
 
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    <FirstApp />
+    <CounterApp value={30} />
   </React.StrictMode>
 );
