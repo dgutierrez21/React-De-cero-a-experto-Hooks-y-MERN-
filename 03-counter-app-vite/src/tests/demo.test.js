da51d7aa712fef3e0ff8df4884115a8f2103c715
Dixon G <dxxn0101@gmail.com>
2023-01-06 09:34:16 -0600

    Jest - Expect - toBe

--- 03-counter-app-vite/src/tests/demo.test.js
+++ 03-counter-app-vite/src/tests/demo.test.js
@@ -1,7 +1,18 @@
-const valor = 5;
+describe("Pruebas en <DemoComponent />", () => {
+  test("Esta prueba no debe fallar", () => {
+    // 1. inicialización
+    const mensaje1 = "Hola mundo";
 
-test("Mi primera prueba", () => {
-  if (valor === 0) {
-    throw new Error("El valor ingresado no puede ser igual a cero.");
-  }
+    // 2. estimulo
+    const mensaje2 = mensaje1.trim();
+
+    // 3. observar el comportamiento esperado
+
+    // expect(valor)
+    // La función expect se utiliza cada vez que se desea comprobar un valor. Rara vez se llama a expect por sí sola. En su lugar, utilizará expect junto con una función "matcher" para afirmar algo sobre un valor.
+
+    // toBe() --- sirve para comparar, en este caso, si mensaje1 es igual a mensaje2
+
+    expect(mensaje1).toBe(mensaje2);
+  });
 });
