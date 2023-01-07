93d87fa4808d1475fab6e494eab99ca99c138dd8
Dixon G <dxxn0101@gmail.com>
2023-01-07 12:47:43 -0600

    Probar FirstApp

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -7,7 +7,7 @@ export const FirstApp = ({ title, subTitle, nombre }) => {
 
   return (
     <>
-      <h1>{title} </h1>
+      <h1> {title} </h1>
       <p>{subTitle} </p>
       <p>{nombre} </p>
     </>
--- 03-counter-app-vite/src/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/src/tests/FirstApp.test.jsx
@@ -4,6 +4,18 @@ import { FirstApp } from "../FirstApp";
 describe("Pruebas en FirstApp", () => {
   test("Debe de hacer match con el snapshot", () => {
     const title = "Hola, soy Goku";
-    render(<FirstApp title={title} />);
+    const { container } = render(<FirstApp title={title} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("Debe mostrar el titulo en un h1", () => {
+    const title = "Hola, soy Goku";
+    const { container, getByText } = render(<FirstApp title={title} />);
+
+    expect(getByText(title)).toBeTruthy();
+
+    const h1 = container.querySelector("h1");
+    expect(h1.textContent).toContain(title);
   });
 });
