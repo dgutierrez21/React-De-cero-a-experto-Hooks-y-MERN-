ea3bde3cae06bd3b1262daa6b8c3c800d048b5a3
Dixon G <dxxn0101@gmail.com>
2023-01-07 20:42:10 -0600

    GifExpertApp - Component

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-
 export const GifExpertApp = () => {
   return (
     <>
+      {/* titulo */}
       <h1>GifExpertApp</h1>
+
+      {/* input */}
+
+      {/* listado de gif */}
+      {/* gif item */}
+      
     </>
   );
 };
