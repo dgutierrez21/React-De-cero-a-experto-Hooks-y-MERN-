fa63906cdda3644df9fa08b5ace4fda90b8bdd33
Dixon G <dxxn0101@gmail.com>
2023-01-09 21:35:13 -0600

    Jest Functions

--- 04-gif-expert-app/tests/components/AgregarCategoria.test.jsx
+++ 04-gif-expert-app/tests/components/AgregarCategoria.test.jsx
@@ -17,7 +17,9 @@ describe("Pruebas en </AgregarCategoria", () => {
   test("Debe de llamar onNuevaCategoria si el input tiene un valor", () => {
     const valorInput = "Saitama";
 
-    render(<AgregarCategoria onNuevaCategoria={() => {}} />);
+    const onNuevaCategoria = jest.fn(); // sumulación de la función
+
+    render(<AgregarCategoria onNuevaCategoria={onNuevaCategoria} />);
 
     const input = screen.getByRole("textbox");
     const form = screen.getByRole("form");
@@ -28,5 +30,22 @@ describe("Pruebas en </AgregarCategoria", () => {
     screen.debug();
 
     expect(input.value).toBe("");
+
+    expect(onNuevaCategoria).toHaveBeenCalled(); // si la función fue llamada
+    expect(onNuevaCategoria).toHaveBeenCalledTimes(1); // si la prueba fue llamada 1 vez
+
+    expect(onNuevaCategoria).toHaveBeenCalledWith(valorInput); // evaluar si ha sido llamada con x valor
+  });
+
+  test("No debe llamar el onNuevaCategoria si el input esta vacio", () => {
+    render(<AgregarCategoria onNuevaCategoria={onNuevaCategoria} />);
+
+    const onNuevaCategoria = jest.fn(); // sumulación de la función
+
+    const form = screen.getByRole("form");
+
+    fireEvent.submit(form);
+
+    expect(onNuevaCategoria).not.toHaveBeenCalled();
   });
 });
