bea1135d3105809aa2a3cb6d1aa327b203700bcf
Dixon G <dxxn0101@gmail.com>
2023-01-10 09:24:00 -0600

    Hacer un mock completo de un Custom Hook

--- 04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -1,13 +1,51 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock("../../src/hooks/useFetchGifs"); // objeto que imita la interfaz y propiedades de una función real, o una clase, o un módulo, o cualquier otro elemento de software
 
 describe("Pruebas en <GifGrid/>", () => {
   const categoria = "One Punch";
   test("Debe mostrar el loading al inicio", () => {
+    useFetchGifs.mockReturnValue({
+      imagenes: [],
+      cargando: true,
+    });
+
     render(<GifGrid categoria={categoria} />);
     screen.debug();
 
     expect(screen.getByText("Cargando.."));
     expect(screen.getByText(categoria));
+  });
+
+  test("Debe mostrar items cuando se cargan las imágenes useFetchGifs", () => {
+    const gifs = [
+      {
+        id: "REI",
+        titulo: "Saitama",
+        url: "https://asfldj/asldf.jpg",
+      },
+      {
+        id: "GKU",
+        titulo: "Goku",
+        url: "https://asfldj/goku.jpg",
+      },
+      {
+        id: "VAL",
+        titulo: "Valorant",
+        url: "https://asfldj/valorant.jpg",
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      imagenes: gifs,
+      cargando: false,
+    });
+
+    render(<GifGrid categoria={categoria} />);
+    screen.debug();
+
+    expect(screen.getAllByRole("img").length).toBe(gifs.length);
   });
 });
