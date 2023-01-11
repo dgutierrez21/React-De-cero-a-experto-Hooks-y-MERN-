5ff9a872acd2b105df27045a3ffd3a8b5abc00b9
Dixon G <dxxn0101@gmail.com>
2023-01-10 20:25:28 -0600

    useEffect - Precauciones

--- 05-hooks-app/src/02-useEffect/Mensaje.jsx
+++ 05-hooks-app/src/02-useEffect/Mensaje.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { useEffect } from "react";
 
 export const Mensaje = () => {
+  const [cords, setCords] = useState({ x: 0, y: 0 });
   useEffect(() => {
     console.log("Mensaje montado");
 
+    const onMouseMove = ({ x, y }) => {
+      //   const cords = { x, y };
+      //   console.log(cords);
+
+      setCords({ x, y });
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
+
     return () => {
-      console.log("Mensaje desmontado");
+      window.removeEventListener("mousemove", onMouseMove);
     };
   }, []);
 
-  return <h3>Usuario ya existe</h3>;
+  return (
+    <>
+      <h3>Usuario ya existe</h3>
+      {JSON.stringify(cords)}
+    </>
+  );
 };
