010c479bb4f87a8003d0b971e2901b88412baee2
Dixon G <dxxn0101@gmail.com>
2023-01-12 17:32:29 -0600

    Borrar un TODO

--- 05-hooks-app/src/09-useReducer/TodoApp.jsx
+++ 05-hooks-app/src/09-useReducer/TodoApp.jsx
@@ -32,6 +32,14 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleDeleteTodo = (id) => {
+    console.log(id);
+    dispatch({
+      type: "Remove Todo",
+      payload: id,
+    });
+  };
+
   return (
     <>
       <h1>TodoApp (10), pendientes: 2</h1>
@@ -39,7 +47,7 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
         </div>
 
         <div className="col-5">
--- 05-hooks-app/src/09-useReducer/components/TodoItem.jsx
+++ 05-hooks-app/src/09-useReducer/components/TodoItem.jsx
@@ -1,8 +1,10 @@
-export const TodoItem = ({ todo: { description } }) => {
+export const TodoItem = ({ todo: { description, id }, onDeleteTodo }) => {
   return (
     <li className="list-group-item d-flex justify-content-between ">
       <span className="align-self-center">{description}</span>
-      <button className="btn btn-danger">Borrar</button>
+      <button className="btn btn-danger" onClick={() => onDeleteTodo(id)}>
+        Borrar
+      </button>
     </li>
   );
 };
