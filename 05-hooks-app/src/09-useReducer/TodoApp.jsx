ceff407f98bb6bf0a621aba4ac33d702098d9871
Dixon G <dxxn0101@gmail.com>
2023-01-12 17:09:45 -0600

    Guardar y Leer TODOs en LocalStorage

--- 05-hooks-app/src/09-useReducer/TodoApp.jsx
+++ 05-hooks-app/src/09-useReducer/TodoApp.jsx
@@ -1,24 +1,27 @@
+import { useEffect } from "react";
 import { useReducer } from "react";
 import { TodoAdd } from "./components/TodoAdd";
 import { TodoList } from "./components/TodoList";
 import { todoReducer } from "./todoReducer";
 
 const initialState = [
-  {
-    id: new Date().getTime(),
-    description: "Recolectar la piedra del alma",
-    done: false,
-  },
-
-  {
-    id: new Date().getTime() * 2,
-    description: "Recolectar la piedra del tiempo",
-    done: false,
-  },
+  //   {
+  //     id: new Date().getTime(),
+  //     description: "Recolectar la piedra del alma",
+  //     done: false,
+  //   },
 ];
 
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos") || []);
+};
+
 export const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const handleNewTodo = (todo) => {
     const action = {
