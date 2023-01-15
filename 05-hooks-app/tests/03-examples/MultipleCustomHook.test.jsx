3e829403e1b45b0a43d65a265ecd0eaaa7fcfbd7
Dixon G <dxxn0101@gmail.com>
2023-01-14 18:45:23 -0600

    Evaluar respuesta del useFetch 01

--- 05-hooks-app/tests/03-examples/MultipleCustomHook.test.jsx
+++ 05-hooks-app/tests/03-examples/MultipleCustomHook.test.jsx
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { MultipleCustomHook } from "../../src/03-examples/MultipleCustomHook";
+import { useFetch } from "../../src/hooks/useFetch";
+
+jest.mock("../../src/hooks/useFetch");
 
 describe("Pruebas en MultipleCustomHook", () => {
   test("Debe mostrar el componente por defecto", () => {
+    useFetch.mockReturnValue({
+      data: null,
+      cargando: true,
+      error: null,
+    });
+
     render(<MultipleCustomHook />);
 
     expect(screen.getByText("Cargando..."));
@@ -13,5 +22,22 @@ describe("Pruebas en MultipleCustomHook", () => {
     expect(nextButton.disabled);
 
     screen.debug();
+  });
+
+  test("Debe mostrar un quote", () => {
+    useFetch.mockReturnValue({
+      data: [{ author: "Francisco", quote: "Hola Mundo" }],
+      cargando: false,
+      error: null,
+    });
+
+    render(<MultipleCustomHook />);
+    screen.debug();
+
+    expect(screen.getByText("Hola Mundo")).toBeTruthy();
+    expect(screen.getByText("Francisco")).toBeTruthy();
+
+    const nextButton = screen.getByRole("button", { name: "Next Quote" });
+    expect(nextButton.disabled).toBeFalsy();
   });
 });
