2e1dcbcdd497e65ee65ade6516fc380ed8c5244f
Dixon G <dxxn0101@gmail.com>
2023-01-17 14:52:32 -0600

    Diseño de la pantalla de búsqueda

--- 07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,41 @@
+import { HeroCard } from "../components";
+
 export const SearchPage = () => {
   return (
-    <h1>searchPage</h1>
-  )
-}
+    <>
+      <h1>Search</h1>
+      <hr />
+      <div className="row animate__animated animate__fadeInUp">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+
+          <form action="">
+            <input
+              type="text"
+              placeholder="Search a Hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+
+            <button className="btn btn-outline-primary mt-3">Search</button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-primary">Search a Hero</div>
+
+          <div className="alert alert-danger">
+            No hero has been found with the word "<b>Doctor Strange</b>"
+          </div>
+
+          {/* <HeroCard  /> */}
+        </div>
+      </div>
+    </>
+  );
+};
