dc31d35340a624569c687ef45c4d5b1812608c37
Dixon G <dxxn0101@gmail.com>
2023-01-18 17:24:54 -0600

    Rutas privadas

--- 07-heroes-spa/src/router/RouterApp.jsx
+++ 07-heroes-spa/src/router/RouterApp.jsx
@@ -5,20 +5,20 @@ import {
 } from "react-router-dom";
 import { LoginPage } from "../auth/pages";
 import { childHeroesRoutes, HeroesRoutes } from "../heroes/routes/HeroesRoutes";
+import { PrivateRoute } from "./PrivateRoute";
 
 const router = createBrowserRouter([
   {
     path: "login",
     element: <LoginPage />,
   },
-
   {
     path: "/",
-    element: <Navigate to="marvel" />,
-  },
-  {
-    path: "/",
-    element: <HeroesRoutes />,
+    element: (
+      <PrivateRoute>
+        <HeroesRoutes />
+      </PrivateRoute>
+    ),
     children: childHeroesRoutes,
   },
 ]);
