4863fa001ed2ea7ee381898b7dde1d050a757ef2
Dixon G <dxxn0101@gmail.com>
2023-01-18 18:17:55 -0600

    Recordar la última página visitada

--- 07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -7,9 +7,11 @@ export const LoginPage = () => {
 
   const navigate = useNavigate();
   const handleLogin = () => {
+    const lastPath = localStorage.getItem("lastPath") || "/marvel";
+
     login("Gabriel");
 
-    navigate("/marvel", {
+    navigate(lastPath, {
       replace: true,
     });
   };
