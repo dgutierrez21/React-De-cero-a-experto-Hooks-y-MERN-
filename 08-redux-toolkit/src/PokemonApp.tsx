1b8201dc1be0496f2f3fcde022df79da5fcf9248
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-15 10:16:12 -0600

    inicio proyecto pokemonApp

---
+++ 08-redux-toolkit/src/PokemonApp.tsx
@@ -0,0 +1,13 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+      <ul>
+        <li>pokemon1</li>
+        <li>pokemon2</li>
+        <li>pokemon3</li>
+      </ul>
+    </>
+  );
+};
