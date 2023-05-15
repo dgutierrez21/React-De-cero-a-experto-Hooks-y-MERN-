be74429a9a460f584a0e2aadcf57259e680b4507
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-15 09:06:33 -0600

    Crear una slice de estado Redux y añadir slice reducer a la store

--- 08-redux-toolkit/src/store/store.ts
+++ 08-redux-toolkit/src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "./slices/counter/counterSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterSlice.reducer,
+  },
 });
 
 // Inferir los tipos `RootState` y `AppDispatch` de la propia tienda
