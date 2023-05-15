f4f7c01378118a157a8622dd41fbdac7648ca6cb
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-15 16:18:43 -0600

    crear thunk

---
+++ 08-redux-toolkit/src/store/slices/pokemon/thunks.ts
@@ -0,0 +1,33 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { pokemonApi } from "../../../api/pokemonApi";
+
+interface Pokemon {
+  page: number;
+  pokemons: any[];
+  isLoading: boolean;
+}
+
+interface rejectWithError {
+  msg: string;
+}
+
+export const getPokemons = createAsyncThunk<
+  Pokemon,
+  number,
+  { rejectValue: rejectWithError }
+>("pokemon/getPokemons", async (page: number, thunkApi) => {
+  try {
+    // TODO: Realizar peticion http
+    const response = await pokemonApi.get(`/pokemon?limit=10&offset=${page}`);
+
+    const data: Pokemon = {
+      isLoading: false,
+      page,
+      pokemons: response.data.results,
+    };
+
+    return thunkApi.fulfillWithValue(data);
+  } catch (error) {
+    return thunkApi.rejectWithValue({ msg: "Error al realizar la conexión" });
+  }
+});
