50c3f4ba3563b943679282fdc103739ee7ea5d4a
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-15 16:24:46 -0600

    disparar thunk en PokemonApp

--- 08-redux-toolkit/src/PokemonApp.tsx
+++ 08-redux-toolkit/src/PokemonApp.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useAppDispatch } from "./store/hooks";
+import { startLoadingPokemons } from "./store/slices/pokemon/pokemonSlice";
+import { getPokemons } from "./store/slices/pokemon/thunks";
+
 export const PokemonApp = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(startLoadingPokemons(true));
+    dispatch(getPokemons(1)).then((resp) => {
+      dispatch(startLoadingPokemons(false));
+    });
+  }, [dispatch]);
+
   return (
     <>
       <h1>PokemonApp</h1>
